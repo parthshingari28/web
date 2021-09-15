c9b82a1de83a672f65e5db316a67bed92ab8fc2b
Parth Shingari <parthshingari14@gmail.com>
2021-09-15 20:45:35 +0530

    Fixed FacultyData.js

--- src/data/FacultyData.js
+++ src/data/FacultyData.js
@@ -4,11 +4,13 @@ export const FacultyData = [
         designation: "Faculty Coordinator",
         imageURL: "/teamImages/faculty/Monit.jpg",
         LinkedIn: "https://www.linkedin.com/in/dr-monit-kapoor-55805677/?originalSubdomain=in",
+        Twitter: "https://twitter.com/monit_kapoor?s=09",
     },
     {
         name: "Dr. Gourav Bathla",
         designation: "Faculty Coordinator",
         imageURL: "/teamImages/faculty/Gourav.jpg",
         LinkedIn: "https://www.linkedin.com/in/dr-gourav-bathla-b2155913/?originalSubdomain=in",
+        Twitter: "https://twitter.com/gouravbathla?s=09",
     },
 ];
